020e4a3de990fb4f4048b6404216f07cfe5a53c3
Edoardo Morandi <morandidodo@gmail.com>
2017-04-12 19:01:51 +0200

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name="HatsPil",
-    version="0.1.1rc3",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "cutadapt",
