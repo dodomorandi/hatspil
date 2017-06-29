dodomorandi/hatspil

commit bed8d16d8f8e90780de53fc3e0654bf478a68cce

Changed output for variant_calling
Now the output files from the variant calling are a bit different.
The whole annotation includes the values from the variants.csv.
Moreover, a "pretty" csv is generated. This file only contains columns
that can be considered interesting without losing anything meaningful
for a human being.

hatspil/variant_calling.py
@@ -258,7 +258,30 @@ class VariantCalling:
         annotation["in_gene_panel"] = False
         annotation.loc[set([index[0] for index in overlaps]), "in_gene_panel"] = True
         annotation["druggable"] = annotation["Gene.refGene"].isin(panel_drug.gene_symbol)
-        annotation.to_csv(os.path.join(self.analysis.get_out_dir(), self.analysis.basename + "_variants.csv"), index=False)
+
+        self.variants = pd.read_csv(self.variants_filename, index_col=False)
+        annotation.reset_index(inplace=True, drop=True)
+        self.variants.reset_index(inplace=True, drop=True)
+
+        annotation_all = annotation.join(self.variants.loc[
+            pd.match(annotation.id, self.variants.key)].drop("key", axis=1))
+        annotation_all.to_csv(os.path.join(
+            self.analysis.get_out_dir(),
+            self.analysis.basename + "_variants.csv"), index=False)
+        annotation_all[[
+            "id", "Chr", "Start", "End", "Ref", "Alt", "Func.refGene",
+            "Gene.refGene", "GeneDetail.refGene", "ExonicFunc.refGene",
+            "AAChange.refGene", "snp138", "cosmic70", "CLINSIG", "CLNDBN",
+            "CLNACC", "CLNDSDB", "CLNDSDBID", "1000G_ALL", "CADD13_PHRED",
+            "gene_type", "damaging", "hgnc_refseq_accession",
+            "hgnc_canonical_refseq", "alternative_refseq", "druggable", "DP",
+            "FREQ", "method"
+        ]].to_csv(os.path.join(self.analysis.get_out_dir(),
+                               self.analysis.basename +
+                               "_variants_pretty.csv"),
+                  index=False)
+
+        del annotation_all
 
         config = self.analysis.config
         if config.use_mongodb:
@@ -330,6 +353,6 @@ class VariantCalling:
 
     def run(self):
         if len(self.mutect_filenames) + sum([len(values) for values in self.varscan_filenames.values()]) > 0:
-            self.prepare_for_annovar()
-            self.annovar()
+            #self.prepare_for_annovar()
+            #self.annovar()
             self.collect_annotated_variants()

commit a28435ccb55e72e1a46742df7580bb3a9c39b9cd

Fixed bug introduced in bed8d16
Two commented lines which should not be commented

hatspil/variant_calling.py
@@ -353,6 +353,6 @@ class VariantCalling:
 
     def run(self):
         if len(self.mutect_filenames) + sum([len(values) for values in self.varscan_filenames.values()]) > 0:
-            #self.prepare_for_annovar()
-            #self.annovar()
+            self.prepare_for_annovar()
+            self.annovar()
             self.collect_annotated_variants()