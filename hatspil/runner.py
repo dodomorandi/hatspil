6e63e512f1a16c2b3cc9bf23d3a2323462a2e2b6
Edoardo Morandi <morandidodo@gmail.com>
2017-12-19 09:48:52 +0100

    FIXED: run strelka only with normals
    If normal is not available for a sample, Strelka must not be run

--- hatspil/runner.py
+++ hatspil/runner.py
@@ -66,7 +66,7 @@ class Runner:
         mutect.run()
         varscan.run()
 
-        if use_strelka:
+        if use_strelka and analysis.using_normals:
             strelka = Strelka(analysis)
             strelka.run()
 
