dodomorandi/hatspil

commit 97bb124015259c70d5e29dc572adc72034975635

Changed xenograft reports out dir

hatspil/xenograft.py
@@ -18,7 +18,7 @@ class Xenograft:
             f"{analysis.config.xenome} classify "\
             f"-T {analysis.config.xenome_threads} "\
             f"-P {analysis.config.xenome_index} --pairs"
-        reports_dir = os.path.join(self.fastq_dir, "REPORTS")
+        reports_dir = os.path.join(self.analysis.bam_dir, "REPORTS")
         try:
             os.makedirs(reports_dir, exist_ok=True)
         except OSError:

commit da35751bcd463efee8acca960cebe1a8608037b2

Fixes problem introduced in 97bb124
`get_bam_dir` must be used instead of `bam_dir`

hatspil/xenograft.py
@@ -18,7 +18,7 @@ class Xenograft:
             f"{analysis.config.xenome} classify "\
             f"-T {analysis.config.xenome_threads} "\
             f"-P {analysis.config.xenome_index} --pairs"
-        reports_dir = os.path.join(self.analysis.bam_dir, "REPORTS")
+        reports_dir = os.path.join(self.analysis.get_bam_dir(), "REPORTS")
         try:
             os.makedirs(reports_dir, exist_ok=True)
         except OSError: