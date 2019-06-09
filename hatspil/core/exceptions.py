7b9585b15d7929ace8b33a5081e478113ea7c3fc
Edoardo Morandi <morandidodo@gmail.com>
2019-06-09 14:51:29 +0200

    Added documentation for exceptions module

--- hatspil/core/exceptions.py
+++ hatspil/core/exceptions.py
@@ -1,14 +1,21 @@
+"""A small set of custom exceptions."""
+
+
 class PipelineError(RuntimeError):
-    pass
+    """An exception generated during pipeline execution.
+    
+    Generally this exception is raised within `Executor` in case of
+    errors.
+    """
 
 
 class BarcodeError(RuntimeError):
-    pass
+    """An exception generated in case of invalid barcoded filenames."""
 
 
 class DataError(RuntimeError):
-    pass
+    """A generic exception for invalid data."""
 
 
 class AnnotationError(RuntimeError):
-    pass
+    """An exception generated in case of invalid genomic annotation."""
